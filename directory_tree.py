84c3a6ea05cf3a7890245baa64c8f2cb504d398b
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2025-02-08 14:45:40 +0200

    master

--- directory_tree.py
+++ directory_tree.py
@@ -13,29 +13,45 @@ logging.basicConfig(level=logging.INFO,
 def get_list(location, config_file: str=APP_CONF) -> list:
     with open(config_file, "r") as f:
         for l in f:
-            linux_obj = l.strip()
-            logging.info('Processing object: %s', linux_obj)
-            if os.path.isfile(linux_obj):
-                execute_file(linux_obj)
-            elif os.path.isdir(linux_obj):
-                check_create_dir(location, linux_obj)
-            else:
-                logging.info('Not suitable object %s', linux_obj)
+            try:
+                linux_obj = l.strip()
+                logging.info('Processing object: %s', linux_obj)
+                if os.path.isfile(linux_obj):
+                    execute_file(linux_obj)
+                elif os.path.isdir(linux_obj):
+                    check_create_dir(location, linux_obj)
+                else:
+                    logging.info('Not suitable object %s', linux_obj)
+            except FileNotFoundError as e:
+                logging.error('file %s doesnt exist', e)
+            except PermissionError as e:
+                logging.error('user %s doesnt have access', e)
+            except Exception as e:
+                logging.error('unexpected exception: %s', e)
 
 def check_create_dir(location: str, dirname: str) -> None:
-    logging.info('Creating dirs: %s', dirname)
-    os.makedirs(f'{location}/{os.path.basename(dirname)}', exist_ok=True)
+    try:
+        logging.info('Creating dirs: %s', dirname)
+        os.makedirs(f'{location}/{os.path.basename(dirname)}', exist_ok=True)
+    except OSError as e:
+        logging.error('cannot create directory due to error: %s', e)
 
 def execute_file(filename: str) -> None:
-    if os.path.splitext(filename)[1] == ".sh":
-        logging.info('Executing file: %s', filename)
-        subprocess.run(filename, capture_output=True)
+    try:
+        if os.path.splitext(filename)[1] == ".sh":
+            logging.info('Executing file: %s', filename)
+            subprocess.run(filename, capture_output=True)
+    except subprocess.SubprocessError as e:
+        logging.error('cannot run process due to error: %s', e)
 
 def main():
     parser = argparse.ArgumentParser(description='Program capturs files and folders and executes files while creating folders')
     parser.add_argument('location')           # positional argument
     args = parser.parse_args()
-    get_list(args.location)
+    try:
+        get_list(args.location)
+    except Exception as e:
+        logging.error(f"Unexpected error in main execution: {e}")
 
 if __name__=="__main__":
     main()
