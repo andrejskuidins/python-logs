4b6de606b06f5e8308866ae10181a9ffe2dfe4cd
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2025-02-08 11:07:53 +0200

    master

--- directory_tree.py
+++ directory_tree.py
@@ -1,16 +1,25 @@
 import os
 import subprocess
+import logging
+
+APP_LOG = 'directory_tree.log'
+
+logging.basicConfig(level=logging.INFO,
+                    handlers=[logging.StreamHandler(),
+                    logging.FileHandler(APP_LOG)])
 
 def get_list(config_file: str="directory_tree.conf") -> list:
     with open(config_file, "r") as f:
         for l in f:
-            if os.is_file(l):
+            if os.path.isfile(l):
                 execute_file(l)
-            else:
+            elif os.path.isdir(l):
                 check_create_dir(l)
+            else:
+                logging.info('Not suitable object')
 
-def check_create_dir(filename: str) -> None:
-    os.makedirs(os.path.basename(filename), )
+def check_create_dir(dirname: str) -> None:
+    os.makedirs(os.path.basename(dirname), )
 
 def execute_file(filename: str) -> None:
     if os.path.splitext(filename)[1] == ".sh":
@@ -18,3 +27,5 @@ def execute_file(filename: str) -> None:
 
 
 get_list()
+
+
