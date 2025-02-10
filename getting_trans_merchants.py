cb13154a0fc07d9b44489325dd133afb4b89d63b
Andrejs.Kuidins <andrejs.kuidins@1nce.com>
2025-02-10 13:36:51 +0200

    master

--- getting_trans_merchants.py
+++ getting_trans_merchants.py
@@ -1,52 +1,58 @@
 import requests
-import json
+import argparse
 import logging
-
-logging(..............)
+import os
 
 ULR_MERCH = 'https://simpledebit.gocardless.io/merchants'
 TRANS_API = 'https://simpledebit.gocardless.io/merchants/'
-PAYMENTS = 'payments.csv'
-
-def get_api_mechants(url: str) -> None:
-    r = requests.get(url)
-    for i in r.json():
-        try:
-            get_api_transactions(i)
-        except:
+PAYMENTS_LOG = 'payments.log'
 
 
-def get_api_transactions(merchant: str) -> None:
-    try:
-        full_api = TRANS_API + merchant
-        r = requests.get(full_api)
-        if r.status_code == 200:
-            print(r.json()['transactions'])
+logging.basicConfig(level=logging.INFO,
+                    handlers=[
+                        logging.StreamHandler(),
+                        logging.FileHandler(PAYMENTS_LOG)
+                    ])
 
-            write_file(PAYMENTS, r.json()['id'], r.json()['iban'], transactions_calc(r.json()['transactions']))
-        else:
-            write_file("no response")
-    except:
 
-def transactions_calc(trans_list: list) -> int:
+
+def write_file(filename: str, iban: str, transactions: int) -> None:
+    with open(filename, "a") as f:
+        f.write(f'{iban},{str(transactions)}\n')
+
+def transactions_calc(trans_list: list, discount: int) -> int:
     calc_of_trans = 0
     for i in trans_list:
         calc_of_trans += int(i["amount"])
         calc_of_trans -= int(i["fee"])
-    return calc_of_trans
+    return round(calc_of_trans*(discount/100))
 
-def write_file(filename: str, id: str, iban: str, transactions: int) -> None:
-    with open(filename, "a") as f:
-        f.write(f'{iban},{str(transactions)}\n')
+def get_api_transactions(merchant: str, filename: str) -> None:
+    try:
+        full_api = TRANS_API + merchant
+        r = requests.get(full_api)
+        if r.status_code == 200:
+            write_file(filename, r.json()['iban'], transactions_calc(r.json()['transactions'], r.json()['discount']['fees_discount']))
+        else:
+            write_file("no response")
+    except PermissionError as e:
+        logging.error(f'Permission error: {e}')
+
+def get_api_mechants(url: str, filename: str) -> None:
+    r = requests.get(url)
+    if os.path.exists(filename):
+        os.remove(filename)
+    for i in r.json():
+        try:
+            get_api_transactions(i, filename)
+        except Exception as e:
+            logging.error(f'Unknown error: {e}')
 
 def main():
-    parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
-    parser.add_argument('filename')           # positional argument
-    get_api_mechants(ULR_MERCH)
-
+    parser = argparse.ArgumentParser(description='Program captures merchants and calculates heir balance with otput to a file payments.csv')
+    parser.add_argument('filename', nargs='?', default='payments.csv')
+    args = parser.parse_args()
+    get_api_mechants(ULR_MERCH, args.filename)
 
 if __name__=="__main__":
     main()
