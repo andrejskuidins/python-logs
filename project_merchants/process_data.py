4dc52da94ee2db08ea9db48c2b2af6074c1375be
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2025-03-02 08:45:28 +0200

    master

--- project_merchants/process_data.py
+++ project_merchants/process_data.py
@@ -1,7 +1,7 @@
 import requests
 import logging
 import time
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 MERCHANT_URL = 'http://127.0.0.1:5000/merchants'
 LOGGING_LOG = 'merchant.log'
@@ -11,31 +11,33 @@ logging.basicConfig(level=logging.INFO,
                     handlers=[logging.StreamHandler(),
                               logging.FileHandler(LOGGING_LOG)])
 
-def merchant_processor(url: str) -> None:
-    with open(PAYMENTS_OUT, "w") as f:
-        f.write("iban,amount_in_pence\n")
-    merchants = requests.get(url)
-    for i in merchants.json():
-        get_merchant_trans(i.strip(), url)
-
-
-def get_merchant_trans(merchant: str, url: str) -> str:
+def merchant_processor(merchant: str) -> None:
     sum_amount = 0
     sum_fees = 0
-    r = requests.get(url + "/" + merchant)
+    r = requests.get(f'{MERCHANT_URL}/{merchant}')
     for i in r.json()["transactions"]:
         sum_amount += i["amount"]
         sum_fees   += i["fee"]
     write_transactions(r.json()["iban"], sum_amount - sum_fees)
 
+def init_out_file() -> None:
+    with open(PAYMENTS_OUT, "w") as f:
+        f.write("iban,amount_in_pence\n")
 
 def write_transactions(iban: str, amount: int) -> None:
     with open(PAYMENTS_OUT, "a") as f:
-        f.write(iban + "," + str(amount) + "\n")
+        f.write(f'{iban},{str(amount)}\n')
 
 if __name__=="__main__":
     start_time = time.time()
-    merchant_processor(MERCHANT_URL)
+
+    init_out_file()
+
+    merchants = requests.get(MERCHANT_URL).json()
+
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        executor.map(merchant_processor, merchants)
+
     end_time = time.time()  # Record the end time
     elapsed_time = end_time - start_time  # Calculate elapsed time
 
