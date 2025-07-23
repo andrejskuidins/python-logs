b82ba5ed243a34bf66cc3f6b2af31d1394f6b8bc
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2025-07-23 22:14:19 +0300

    JIRA-01-hanoi

--- hanoitowers/hanoi_towers.py
+++ hanoitowers/hanoi_towers.py
@@ -7,9 +7,7 @@ stacks = []
 left_stack = Stack("Left")
 middle_stack = Stack("Middle")
 right_stack = Stack("Right")
-stacks.append(left_stack)
-stacks.append(middle_stack)
-stacks.append(right_stack)
+stacks += [left_stack, middle_stack, right_stack]
 #Set up the Game
 num_disks = int(input("\nHow many disks do you want to play with?\n"))
 while num_disks < 3:
