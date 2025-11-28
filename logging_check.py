627876d6093aa891a4c531ce9c49f34b5fa1b3b2
Andrejs Kuidins, SUB-SDC-LV-DNA-DL <andrejs.kuidins@swisscom.com>
2025-11-28 12:17:02 +0200

    feature/jira-05-adjust

--- logging_check.py
+++ logging_check.py
@@ -1,18 +1,29 @@
 import logging
 import sys
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("formatted.log")
-stream_handler = logging.StreamHandler(sys.stdout)
-formatter1 = logging.Formatter(
-    "[%(asctime)s] {%(levelname)s} %(name)s: #%(lineno)d - %(message)s"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("basic_config.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
 )
-formatter2 = logging.Formatter("[%(asctime)s] {%(levelname)s} - %(message)s")
-file_handler.setFormatter(formatter1)
-stream_handler.setFormatter(formatter2)
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+
+logger = logging.getLogger(__name__)
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# file_handler = logging.FileHandler("formatted.log")
+# stream_handler = logging.StreamHandler(sys.stdout)
+# formatter1 = logging.Formatter(
+#     "[%(asctime)s] {%(levelname)s} %(name)s: #%(lineno)d - %(message)s"
+# )
+# formatter2 = logging.Formatter("[%(asctime)s] {%(levelname)s} - %(message)s")
+# file_handler.setFormatter(formatter1)
+# stream_handler.setFormatter(formatter2)
+# logger.addHandler(file_handler)
+# logger.addHandler(stream_handler)
 
 
 def division():
