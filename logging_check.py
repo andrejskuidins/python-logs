dd61a9e248abe0d59d90fff6d00b1aeee892a8da
Andrejs Kuidins, SUB-SDC-LV-DNA-DL <andrejs.kuidins@swisscom.com>
2025-11-28 12:09:54 +0200

    feature/jira-05-adjust

--- logging_check.py
+++ logging_check.py
@@ -2,25 +2,32 @@ import logging
 import sys
 
 logger = logging.getLogger(__name__)
-stream_handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(stream_handler)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("output.log")
+file_handler = logging.FileHandler("formatted.log")
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter1 = logging.Formatter(
+    "[%(asctime)s] {%(levelname)s} %(name)s: #%(lineno)d - %(message)s"
+)
+formatter2 = logging.Formatter("[%(asctime)s] {%(levelname)s} - %(message)s")
+file_handler.setFormatter(formatter1)
+stream_handler.setFormatter(formatter2)
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def division():
+    logger.debug("Starting Division!")
     try:
         dividend = float(input("Enter the dividend: "))
-        logger.info(f"value is {dividend}")
+        logger.info(dividend)
         divisor = float(input("Enter the divisor: "))
-        logger.info(f"value is {divisor}")
-    except ValueError:
-        logger.critical("No dividend or divisor value entered!")
-        return None
+        logger.info(divisor)
+    except SyntaxError:
+        logger.log(logging.CRITICAL, "No dividend or divisor value entered!")
+        return
     if divisor == 0:
         logger.error("Attempting to divide by 0!")
-        return None
+        return
     else:
         return dividend / divisor
 
