4c5e38d3631e79f2ead31dc0b4469f2888d34dcb
Andrejs.Kuidins <andrejs.kuidins@1nce.com>
2025-11-30 16:58:44 +0200

    feature/jira-05-adjust

--- udemy.py
+++ udemy.py
@@ -1,3 +1,17 @@
+def best_scores(lst):
+    max1, max2 = 0, 0
+    for i in lst:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2 and i != max1:
+            max2 = i
+    return max1, max2
+
+
+myList = [84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0]
+print(best_scores(myList))  # 90 87
+
 # def missing_number2(arr, n):
 #     total = n * (n + 1) // 2
 #     sum_arr = sum(arr)
@@ -22,9 +36,22 @@
 # print(return_pairs([3, 3], 6))
 
 
-def max_product(arr):
-    arr.sort()
-    return arr[-2] * arr[-1]
+# def max_product(arr):
+#     arr.sort()
+#     return arr[-2] * arr[-1]
 
 
-print(max_product([1, 7, 3, 4, 9, 5]))
+# def max_product(arr):
+#     max1, max2 = 0, 0
+#     for i in arr:
+#         print(max1, max2)
+#         if i > max1:
+#             max2 = max1
+#             max1 = i
+#         elif i > max2 and i != max1:
+#             max2 = i
+#     return max1, max2
+
+
+# print(max_product([1, 7, 3, 4, 9, 5]))
+# print(max_product([1, 7, 3, 4, 9, 5, 1, 10]))
