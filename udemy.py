295e091cdf955724ecb231a17f7aaccb3963af80
Andrejs.Kuidins <andrejs.kuidins@1nce.com>
2025-12-01 21:45:48 +0200

    feature/jira-05-adjust

--- udemy.py
+++ udemy.py
@@ -1,16 +1,47 @@
-def best_scores(lst):
-    max1, max2 = 0, 0
-    for i in lst:
-        if i > max1:
-            max2 = max1
-            max1 = i
-        elif i > max2 and i != max1:
-            max2 = i
-    return max1, max2
+def rotate(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            print(matrix)
+
+    for i in range(n):
+        matrix[i].reverse()
+    return matrix
 
 
-myList = [84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0]
-print(best_scores(myList))  # 90 87
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(rotate(matrix))
+# Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+# def pair_sum(arr, target):
+#     output = set()
+#     for i in range(len(arr)):
+#         for j in range(i + 1, len(arr)):
+#             if arr[i] + arr[j] == target:
+#                 if arr[i] >= arr[j]:
+#                     output.add((arr[i], arr[j]))
+#                 elif arr[i] < arr[j]:
+#                     output.add((arr[j], arr[i]))
+#     return [f"{b}+{a}" for (a, b) in output]
+
+
+# print(pair_sum([2, 4, 3, 5, 6, -2, 4, 7, 8, 9], 7))
+
+# def remove_duplicates(arr):
+#     output = []
+#     seen = set()
+#     for i in arr:
+#         if i not in seen:
+#             seen.add(i)
+#             output.append(i)
+#     return output
+
+
+# print(remove_duplicates([1, 1, 2, 2, 3, 4, 5]))
+# print(remove_duplicates([5, 1, 2, 5, 1]))
+
 
 # def missing_number2(arr, n):
 #     total = n * (n + 1) // 2
