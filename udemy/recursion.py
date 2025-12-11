ddbe67fc7e0052cf267d6ff5b108372d9e96c75f
Andrejs Kuidins, SUB-SDC-LV-DNA-DL <andrejs.kuidins@swisscom.com>
2025-12-11 14:15:24 +0200

    feature/jira-05-adjust

--- udemy/recursion.py
+++ udemy/recursion.py
@@ -1,22 +1,97 @@
-def capitalizeFirst(arr):
-    if len(arr) == 0:
-        return []
-    return list(arr[0].capitalize()) + capitalizeFirst(arr[1:])
+def collectStrings(obj):
+    resultDict = []
+    for v in obj.values():
+        if isinstance(v, dict):
+            resultDict.extend(collectStrings(v))
+        elif type(v) == str:
+            resultDict.append(v)
+    return resultDict
 
 
-print(capitalizeFirst(["car", "taco", "banana"]))  # ['Car','Taco','Banana']
+obj = {
+    "stuff": "foo",
+    "data": {
+        "val": {
+            "thing": {"info": "bar", "moreInfo": {"evenMoreInfo": {"weMadeIt": "baz"}}}
+        }
+    },
+}
+
+print(collectStrings(obj))  # ['foo', 'bar', 'baz']
+
+
+# def stringifyNumbers(obj):
+#     resultDict = {}
+#     for k, v in obj.items():
+#         if isinstance(v, dict):
+#             resultDict[k] = stringifyNumbers(v)
+#         elif type(v) == int:
+#             resultDict[k] = str(v)
+#         else:
+#             resultDict[k] = v
+#     return resultDict
+
+
+# obj = {
+#     "num": 1,
+#     "test": [],
+#     "data": {"val": 4, "info": {"isRight": True, "random": 66}},
+# }
+
+# print(stringifyNumbers(obj))
+
+# def nestedEvenSum(obj, sum=0):
+#     for element in obj.values():
+#         if isinstance(element, dict):
+#             sum += nestedEvenSum(element)
+#         elif isinstance(element, int) and element % 2 == 0:
+#             sum += element
+#     return sum
+
+
+# obj1 = {
+#     "outer": 2,
+#     "obj": {
+#         "inner": 2,
+#         "otherObj": {"superInner": 2, "notANumber": True, "alsoNotANumber": "yup"},
+#     },
+# }
+
+# obj2 = {
+#     "a": 2,
+#     "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+#     "c": {"c": {"c": 2}, "cc": "ball", "ccc": 5},
+#     "d": 1,
+#     "e": {"e": {"e": 2}, "ee": "car"},
+# }
+
+# print(nestedEvenSum(obj1))  # 6
+# print(nestedEvenSum(obj2))  # 10
+
 
 # def flatten(arr):
-#     print(arr)
-#     if type(arr) == type(int):
-#         return arr
-#     return flatten(arr[1:])
+#     resultArr = []
+#     for custItem in arr:
+#         if type(custItem) is list:
+#             resultArr.extend(flatten(custItem))
+#         else:
+#             resultArr.append(custItem)
+#     return resultArr
 
 
 # print(flatten([1, 2, 3, [4, 5]]))  # [1, 2, 3, 4, 5]
 # print(flatten([1, [2, [3, 4], [[5]]]]))  # [1, 2, 3, 4, 5]
 # print(flatten([[1], [2], [3]]))  # [1, 2, 3]
 # print(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]))  # [1, 2, 3]
+
+
+# def capitalizeFirst(arr):
+#     if len(arr) == 0:
+#         return []
+#     return [arr[0].capitalize()] + capitalizeFirst(arr[1:])
+
+
+# print(capitalizeFirst(["car", "taco", "banana"]))  # ['Car','Taco','Banana']
 
 # def isOdd(num):
 #     if num % 2 == 0:
